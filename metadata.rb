aa0de80c28cba26d2a9468397631a8f245708c22
Andrew Forward <aforward@gmail.com>
2013-06-11 15:45:29 -0400

    updated maintainer info

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "monit"
-maintainer       "Alex Soto"
-maintainer_email "apsoto@gmail.com"
+maintainer       "Andrew Forward"
+maintainer_email "aforward@gmail.com"
 license          "MIT"
 description      "Configures monit.  Originally based off the 37 Signals Cookbook."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.7.1"
+version          "0.8.0"
 
 
 attribute 'monit/notify_email', 
